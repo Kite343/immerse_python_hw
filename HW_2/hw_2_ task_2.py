fa4de41450fcfa1de842d37809c76902ebc7afcd
Morozova Kate <morozova1861@mail.ru>
2023-06-10 23:06:55 +0600

    fixed hw_2_task_2

--- HW_2/hw_2_ task_2.py
+++ HW_2/hw_2_ task_2.py
@@ -11,22 +11,32 @@ def task_action(n_1, n_2):
     a = n_1[0] * n_2[1] + n_1[1] * n_2[0]
     b = n_1[1] * n_2[1]
     nod = math.gcd(a, b)
-    n_res = (a // nod, b // nod)
-    return n_res
+    a //= nod
+    b //= nod
+    if b == 1:
+        n_res_1 = str(a)
+    else:
+        n_res_1 = str(a) + "/" + str(b)
+    a = n_1[0] * n_2[0]
+    b = n_1[1] * n_2[1]
+    nod = math.gcd(a, b)
+    a //= nod
+    b //= nod
+    if b == 1:
+        n_res_2 = str(a)
+    else:
+        n_res_2 = str(a) + "/" + str(b)
+    return n_res_1, n_res_2
 
 def input_fracthion_num():
-    print("Введите числитель")
-    a = input_num()
-    print("Введите знаменатель")
-    b = input_num()
-    return (a, b)
-
-def input_num():
     while True:
+        print("Введите дробное число вида a/b (знаменатель не может быть равен 0):")
         num = input()
-        if num.isdigit():
-            return int(num)
-        print("Введите целое число")
+        if "/" in num:
+            num = num.split("/")
+            if len(num) == 2 and \
+            num[0].isdigit() and num[1].isdigit() and int(num[1]) != 0:
+                return  int(num[0]), int(num[1])          
 
 def action():
     while True:
@@ -51,10 +61,17 @@ while start:
             num_2 = input_fracthion_num()
             
             res = task_action(num_1, num_2)
-            res_str = str(res[0]) + "/" + str(res[1])
-            print(res_str)
-            print("Проверка")
+            print(f"сумма равна {res[0]}")
+            print(f"произведение равно {res[1]}")
+
+            print("Проверка суммы")
             chek = fractions.Fraction(num_1[0], num_1[1]) + fractions.Fraction(num_2[0], num_2[1])
-            print(f"{res_str == str(chek)} {chek} = {res_str}")
+            print(f"{res[0] == str(chek)} {chek} = {res[0]}")
+
+            print("Проверка произведения")
+            chek = fractions.Fraction(num_1[0], num_1[1]) * fractions.Fraction(num_2[0], num_2[1])
+            print(f"{res[1] == str(chek)} {chek} = {res[1]}")
+
         case '2':
             start = False
+
